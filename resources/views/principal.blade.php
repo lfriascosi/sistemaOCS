e6e0fd9077c53c6940a5639d3bb7bc7acec796b2
flriascosi <fabricio.riascosi@gmail.com>
2020-11-26 02:30:58 -0500

    Compilacion css-js laravelMix

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -11,10 +11,8 @@
     <link rel="shortcut icon" href="img/logo.png">
     <title>Sistema OCS</title>
     <!-- Icons -->
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/simple-line-icons.min.css" rel="stylesheet">
     <!-- Main styles for this application -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/plantilla.css" rel="stylesheet">
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
@@ -121,14 +119,9 @@
     </footer>
 
     <!-- Bootstrap and necessary plugins -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/pace.min.js"></script>
     <!-- Plugins and scripts required by all views -->
-    <script src="js/Chart.min.js"></script>
     <!-- GenesisUI main scripts -->
-    <script src="js/template.js"></script>
+    <script src="js/plantilla.js"></script>
 </body>
 
 </html>
